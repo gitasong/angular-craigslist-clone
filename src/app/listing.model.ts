fa3eae893fefc66eaede73b5aa934f894d9096bb
Larry Taylor and Nicole Freed <larry@>
2017-08-22 09:34:21 -0700

    create listing model with constructor

---
+++ src/app/listing.model.ts
@@ -0,0 +1,10 @@
+export class Listing {
+  constructor (
+              public title: string,
+              public price: number,
+              public image: string,
+              public description: string,
+              public location: string,
+              public id: number
+              ) { }
+}
