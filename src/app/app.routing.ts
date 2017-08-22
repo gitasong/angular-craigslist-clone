5cc2fd181ef23b7c2179d7a01e48d03e8b0e897f
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 09:17:19 -0700

    add personals component to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { MainComponent } from './main/main.component';
 import { ForSaleComponent } from './for-sale/for-sale.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { HousingComponent } from './housing/housing.component';
+import { PersonalsComponent } from './personals/personals.component';
 
 const appRoutes: Routes = [
   {
@@ -21,6 +22,10 @@ const appRoutes: Routes = [
   {
     path: 'housing',
     component: HousingComponent
+  },
+  {
+    path: 'personals',
+    component: PersonalsComponent
   }
 ];
 
