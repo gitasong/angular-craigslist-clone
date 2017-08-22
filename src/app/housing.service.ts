6a7caed01e63cdbaacb9808feeb525893289495c
Larry Taylor and Nicole Freed <larry@>
2017-08-22 14:24:55 -0700

    build housing-service

---
+++ src/app/housing.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Listing } from './listing.model';
+import { HOUSING } from './housing-data';
+
+@Injectable()
+export class HousingService {
+
+  constructor() {
+    return HOUSING;
+  }
+
+}
