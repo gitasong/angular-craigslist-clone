dc03af813bbf317310d1aa0ac549f93dc2cad138
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 09:06:27 -0700

    add for-sale component to router and Router list

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { ForSaleComponent } from './for-sale/for-sale.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: MainComponent
   },
+  {
+    path: 'for-sale',
+    component: ForSaleComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
