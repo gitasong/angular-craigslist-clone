2aab643c78ac2371820b510d9376cac7819f5db7
Larry Taylor and Nicole Freed <larry@>
2017-08-22 14:10:10 -0700

    import model, router, and jobs service; add to providers and constructor

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing.model';
+import { Router } from '@angular/router';
+import { JobsService } from '../jobs.service';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+  styleUrls: ['./jobs.component.css'],
+  providers: [JobsService]
 })
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private jobsService: JobsService) { }
 
   ngOnInit() {
   }
