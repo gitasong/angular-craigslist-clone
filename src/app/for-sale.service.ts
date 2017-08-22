a52816f935e180a65f2004e4a4667b60f1b4ddd5
Larry Taylor and Nicole Freed <larry@>
2017-08-22 13:31:56 -0700

    import Listing model and for sale data; create getForSale() method

--- src/app/for-sale.service.ts
+++ src/app/for-sale.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Listing } from './listing.model';
+import { FORSALE } from './for-sale-data';
 
 @Injectable()
 export class ForSaleService {
 
   constructor() { }
+
+  getForSale() {
+    return FORSALE;
+  }
 
 }
