af8b0ef288229cc85f4e7c32b82e7eaf4302002e
Larry Taylor and Nicole Freed <larry@>
2017-08-22 14:07:19 -0700

    import model and jobs data; export getJobs() method

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Listing } from './listing.model';
+import { JOBS } from './jobs-data';
 
 @Injectable()
 export class JobsService {
 
   constructor() { }
+
+  getJobs() {
+    return JOBS;
+  }
 
 }
