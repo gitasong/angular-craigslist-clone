00bce073aeeb3a9565223b5a42fc89499ce91329
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 14:38:23 -0700

    inject personals service in personals component

--- src/app/personals/personals.component.ts
+++ src/app/personals/personals.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing.model';
+import { Router } from '@angular/router';
+import { PersonalsService } from '../personals.service';
 
 @Component({
   selector: 'app-personals',
   templateUrl: './personals.component.html',
-  styleUrls: ['./personals.component.css']
+  styleUrls: ['./personals.component.css'],
+  providers: [PersonalsService]
 })
 export class PersonalsComponent implements OnInit {
+  personals: Listing[];
 
-  constructor() { }
+  constructor(private router: Router, private personalsService: PersonalsService) { }
 
   ngOnInit() {
+    this.personals = this.personalsService.getPersonals();
   }
 
+  goToPersonalsPage(clickedListing: Listing) {
+    this.router.navigate(['personals', clickedListing.id]);
+  };
 }
