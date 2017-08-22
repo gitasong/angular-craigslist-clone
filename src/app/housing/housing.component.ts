84fc8d8510f59b4f02ff0812f5848685b26730a9
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 14:28:24 -0700

    add dependency injector parts to housing component

--- src/app/housing/housing.component.ts
+++ src/app/housing/housing.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing.model';
+import { Router } from '@angular/router';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-housing',
   templateUrl: './housing.component.html',
-  styleUrls: ['./housing.component.css']
+  styleUrls: ['./housing.component.css'],
+  providers: [HousingService]
 })
 export class HousingComponent implements OnInit {
+  housing: Listing[];
 
-  constructor() { }
+  constructor(private router: Router, private housingService: HousingService) { }
 
   ngOnInit() {
+    this.housing = this.housingService.getHousing();
   }
 
+  goToHousingPage(clickedListing: Listing) {
+    this.router.navigate(['housing', clickedListing.id]);
+  };
 }
