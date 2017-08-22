7754ab171cbf6472c55637e7cf42f37cf013988e
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 13:35:13 -0700

    register service provider

--- src/app/for-sale/for-sale.component.ts
+++ src/app/for-sale/for-sale.component.ts
@@ -4,7 +4,8 @@ import { ForSaleService } from '../for-sale.service';
 @Component({
   selector: 'app-for-sale',
   templateUrl: './for-sale.component.html',
-  styleUrls: ['./for-sale.component.css']
+  styleUrls: ['./for-sale.component.css'],
+  providers: [ForSaleService]
 })
 export class ForSaleComponent implements OnInit {
 
