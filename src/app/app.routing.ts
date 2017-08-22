73619e33f354c26a88dbb2a93b1d442bf7186e18
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 10:42:13 -0700

    import listing detail component; create dynamic route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { ForSaleComponent } from './for-sale/for-sale.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { HousingComponent } from './housing/housing.component';
 import { PersonalsComponent } from './personals/personals.component';
+import { ListingDetailComponent } from './listing-detail/listing-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -26,6 +27,10 @@ const appRoutes: Routes = [
   {
     path: 'personals',
     component: PersonalsComponent
+  },
+  {
+    path: 'listings/:id',
+    component: ListingDetailComponent
   }
 ];
 
