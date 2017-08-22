73786457c47550768b83414cc66ae715b6b8ed9a
Larry Taylor and Nicole Freed <nicole@>
2017-08-22 15:03:13 -0700

    delete listing details component from router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,6 @@ import { ForSaleComponent } from './for-sale/for-sale.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { HousingComponent } from './housing/housing.component';
 import { PersonalsComponent } from './personals/personals.component';
-import { ListingDetailComponent } from './listing-detail/listing-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -27,10 +26,6 @@ const appRoutes: Routes = [
   {
     path: 'personals',
     component: PersonalsComponent
-  },
-  {
-    path: 'listings/:id',
-    component: ListingDetailComponent
   },
   {
     path: 'for-sale/:id',
